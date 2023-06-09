be2b6e3fb47af19ed70b6b1a941f400cb448cf6d
Pablo Chehade <pablo.chehade.villalba@gmail.com>
2023-06-09 10:15:10 -0300

    Creo los módulos de la librería

---
+++ Breakout/src/MyLibrary/Bloques.fs
@@ -0,0 +1,5 @@
+﻿namespace Bloques
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ Breakout/src/MyLibrary/Interacciones.fs
@@ -0,0 +1,5 @@
+﻿namespace Interacciones
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
