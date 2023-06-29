4bbe066eb1dbda23362fc159bea81798b704d20f
adosil1 <andres.dosil@ib.edu.ar>
2023-06-29 10:35:35 -0300

    Modifico modulo Bloques

--- Breakout/src/MyLibrary/Bloques.fs
+++ Breakout/src/MyLibrary/Bloques.fs
@@ -10,14 +10,17 @@ module Bloques =
     // los bloques estan entre la altura 800 y la altura 1000; limite inferior inclusivo, limite superior exclusivo [800, 1000)
     let yInicial = 800.0
     let yFinal = 1000.0
-    
+
+    type Bloques =
+    {
+        Estado: Map<int*int, bool>
+    }
     // se almacena el estado de los bloques en un mapa cuyas claves son los indices (fila, columna) de cada bloque
     // y cuyos valores son un booleano que indica si el bloque esta activo o no
-    let inicializarBloques (filas: int) (columnas: int) : Map<int*int, bool> =
+    let inicializarBloques (filas: int) (columnas: int) : Bloques =
         let indices = [for x in 0 .. filas - 1 do for y in 0 .. columnas - 1 -> (x, y)]
-        indices
-        |> List.map (fun (x, y) -> ((x, y), true))
-        |> Map.ofList
+        let estado = indices |> List.map (fun (x, y) -> ((x, y), true)) |> Map.ofList
+        { Estado = estado }
 
     // obtiene las coordenadas de la esquina inferior izquierda del bloque a partir de sus indices
     let obtenerCoordenadas (fila: int) (columna: int): float*float =
@@ -26,8 +29,9 @@ module Bloques =
         (x, y)
 
     // cambia el estado de un bloque a false
-    let desactivarBloque (fila: int) (columna: int) (bloques: Map<int*int, bool>) : Map<int*int, bool>=
-        bloques |> Map.add (fila, columna) false
+    let desactivarBloque (fila: int) (columna: int) (bloques: Bloques) : Bloques=
+        let nuevoEstado = bloques.Estado |> Map.add (fila, columna) false
+        { bloques with Estado = nuevoEstado}
 
     let condicion_inicial_bloques = inicializarBloques 8 14
 
