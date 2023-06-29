92cfa695ccf84f7ba3da9ed0dd514898cdda4770
ZoePrieto <zoe.prieto@ib.edu.ar>
2023-06-29 10:44:13 -0300

    Bug solucionado en test de interaccion con bloques

--- Breakout/tests/MyTests/UnitTest1.fs
+++ Breakout/tests/MyTests/UnitTest1.fs
@@ -85,8 +85,18 @@ let ``Interaccion bloques`` () =
     let bloque2:Bloques.Bloque = {x = 5.; y = 0.; Lx = 5.; Ly = 5.}
     let bloques: Bloques.Bloque List = [bloque1; bloque2]
 
+    let bolita1:bolita.Bolita = {x = 50.; y = 50.; vx = 1.; vy = 1.}
+    let bolita2:bolita.Bolita = {x = 4.9; y = 4.9; vx = 0.; vy = 1.}
+    let bolita3:bolita.Bolita = {x = 8.; y = 4.9; vx = 0.; vy = 1.} 
+    let bolita4:bolita.Bolita = {x = 8.; y = 4.9; vx = 140.; vy = 10.}
 
-    Assert.AreEqual( ({x = 50.; y = 50.; vx = 1.; vy = 1.} Interacciones.Bloque_eliminado.NoEliminado), (Interacciones.Interaccion_bloques bloques {x = 50.; y = 50.; vx = 1.; vy = 1.})) //no hay choque con ningún bloque
-    Assert.AreEqual( ({x = 4.9; y = 4.9; vx = 0.; vy = 1.} Interacciones.Bloque_eliminado.Eliminado bloque1), (Interacciones.Interaccion_bloques bloques {x = 4.9; y = 4.9; vx = 0.; vy = -1.})) //choca en el bloque 1
-    Assert.AreEqual( ({x = 8.; y = 4.9; vx = 0.; vy = 1.} Interacciones.Bloque_eliminado.Eliminado bloque2), (Interacciones.Interaccion_bloques bloques {x = 8.; y = 4.9; vx = 0.; vy = -1.})) //choca en el bloque 2
-    Assert.AreEqual( ({x = 8.; y = 4.9; vx = 140.; vy = 10.} Interacciones.Bloque_eliminado.Eliminado bloque2), (Interacciones.Interaccion_bloques bloques {x = 8.; y = 4.9; vx = 140.; vy = -10.})) //choca en el bloque 2 con distinta velocidad
+    Assert.AreEqual( (bolita1,  Interacciones.Bloque_eliminado.NoEliminado),(Interacciones.Interaccion_bloques bloques {x = 50.; y = 50.; vx = 1.; vy = 1.})) //no hay choque con ningún bloque
+    Assert.AreEqual( (bolita2, Interacciones.Bloque_eliminado.Eliminado bloque1),(Interacciones.Interaccion_bloques bloques {x = 4.9; y = 4.9; vx = 0.; vy = -1.})) //choca en el bloque 1
+    Assert.AreEqual( (bolita3, Interacciones.Bloque_eliminado.Eliminado bloque2),(Interacciones.Interaccion_bloques bloques {x = 8.; y = 4.9; vx = 0.; vy = -1.}))  //choca en el bloque 2
+    Assert.AreEqual( (bolita4, Interacciones.Bloque_eliminado.Eliminado bloque2),(Interacciones.Interaccion_bloques bloques {x = 8.; y = 4.9; vx = 140.; vy = -10.})) //choca en el bloque 2 con distinta velocidad
+
+
+    //Assert.AreEqual( [bolita1; Interacciones.Bloque_eliminado.NoEliminado], (Interacciones.Interaccion_bloques bloques {x = 50.; y = 50.; vx = 1.; vy = 1.})) //no hay choque con ningún bloque
+    //Assert.AreEqual( (bolita2  Interacciones.Bloque_eliminado.Eliminado bloque1), (Interacciones.Interaccion_bloques bloques {x = 4.9; y = 4.9; vx = 0.; vy = -1.})) //choca en el bloque 1
+    //Assert.AreEqual( (bolita3  Interacciones.Bloque_eliminado.Eliminado bloque2), (Interacciones.Interaccion_bloques bloques {x = 8.; y = 4.9; vx = 0.; vy = -1.})) //choca en el bloque 2
+    //Assert.AreEqual( (bolita4  Interacciones.Bloque_eliminado.Eliminado bloque2), (Interacciones.Interaccion_bloques bloques {x = 8.; y = 4.9; vx = 140.; vy = -10.})) //choca en el bloque 2 con distinta velocidad
